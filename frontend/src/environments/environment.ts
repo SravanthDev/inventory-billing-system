0d19e136b7a8088a1d591cacde7387715a1c63af
SravanthDev <sravanth7762@gmail.com>
2025-11-03 00:44:44 +0530

    Fixed double /api issue

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://inventory-billing-system-v4bi.onrender.com/api'
+  apiUrl: 'https://inventory-billing-system-v4bi.onrender.com'
 
 };
 
