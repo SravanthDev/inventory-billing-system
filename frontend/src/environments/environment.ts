dad7271aeae2e96e167205e830eae1760f556760
SravanthDev <sravanth7762@gmail.com>
2025-11-02 23:38:16 +0530

    backend deployed

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api'
+  production: true,
+  apiUrl: 'https://inventory-billing-system-v4bi.onrender.com/'
+
 };
 
